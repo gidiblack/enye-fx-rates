73918024a3b5bab27c608dbadfef4c37bb01befc
Gideon Etim <gidiblack@gmail.com>
2021-01-19 17:44:36 +0100

    project init

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+// const querystring = require('querystring');
+// const url = require('url');
+
+let app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const api_url = `https://api.exchangeratesapi.io/latest`;
+
+// Function to handle the root path
+app.get('/', async (req, res) => {
+  res = await fetch(api_url);
+  const json = await res.json();
+  console.log(json);
+});
+
+app.listen(8000, function() {
+  console.log('Server is listening on port 8000')
+});
